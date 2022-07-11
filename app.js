acb59ebe2247f16e605633c191772249ccfa25af
HumnaIzhar <107610302+HumnaIzhar@users.noreply.github.com>
2022-07-11 16:38:02 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+ var list = document.getElementById('list');
+function add(){
+    var todo_item = document.getElementById('input');
+if(todo_item.value !=""){
+    var li = document.createElement('li')
+    var liText = document.createTextNode(todo_item.value)
+    var img = document.createElement('img')
+    img.setAttribute('src','img/edit.png')
+    img.setAttribute("class","img")
+    img.setAttribute("onclick","imagedelete(this)")
+    var edit = document.createElement('button')
+    edit.setAttribute("onclick","editimage(this)")
+    var edittxt = document.createTextNode('Edit')
+    li.append(liText)
+    edit.append(edittxt)
+    li.appendChild(edit)
+    li.appendChild(img)
+    list.appendChild(li)
+    todo_item.value=""
+}  
+else{
+    alert("Error")
+}
+}
+function imagedelete(e){
+    e.parentNode.remove()
+}
+function editimage(e) {
+    var inputvalue = prompt("Enter ToDo", e.parentNode.firstChild.nodeValue)
+    e.parentNode.firstChild.nodeValue = inputvalue
+}
+function del(){
+    list.innerHTML = ""
+}
